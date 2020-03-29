be5b11731c0c7c0976a83a45c3d8c2dbfdae642d
gilles loriquer <gilles.loriquer@gfi.fr>
2020-03-29 13:14:19 +0200

    Identification des URLs de recherche et de détail d'une recette

--- app/src/main/java/com/example/foodrecipes/network/RecipeApi.java
+++ app/src/main/java/com/example/foodrecipes/network/RecipeApi.java
@@ -1,4 +1,6 @@
 package com.example.foodrecipes.network;
 
 public interface RecipeApi {
+    // get recipe detail URL = https://recipesapi.herokuapp.com/api/get?rId=47070
+    // get recipes search URL = https://recipesapi.herokuapp.com/api/search?q=chicken&page=2
 }
