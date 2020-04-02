d39acf408f7cc01420c19a915fe65e0b3cc9011b
gilles loriquer <gilles.loriquer@gfi.fr>
2020-04-02 11:13:15 +0200

    Création d'une class générique pour la gestion des réponses rétrofit

---
+++ app/src/main/java/com/example/foodrecipes/network/response/ApiResponse.java
@@ -0,0 +1,64 @@
+package com.example.foodrecipes.network.response;
+
+import java.io.IOException;
+
+import retrofit2.Response;
+
+public class ApiResponse<T> {
+
+    public ApiResponse<T> create(Throwable error) {
+        return new ApiErrorResponse<>(!error.getMessage().equals("") ? error.getMessage() : "" +
+                "Unknown error\nCheck network connection");
+    }
+
+    public ApiResponse<T> create(Response<T> response) {
+        if (response.isSuccessful()) {
+            T body = response.body();
+
+            // 204 : empty response
+            if (body == null || response.code() == 204) {
+                return new ApiEmptyResponse<>();
+            } else {
+                return new ApiSuccessResponse<>(body);
+            }
+        } else {
+            String errorMsg;
+            try {
+                errorMsg = response.errorBody().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+                errorMsg = response.message();
+            }
+            return new ApiErrorResponse<>(errorMsg);
+        }
+    }
+
+    public class ApiSuccessResponse<T> extends ApiResponse<T> {
+
+        private T body;
+
+        public ApiSuccessResponse(T body) {
+            this.body = body;
+        }
+
+        public T getBody() {
+            return body;
+        }
+    }
+
+    public class ApiErrorResponse<T> extends ApiResponse<T> {
+
+        private String errorMessage;
+
+        public ApiErrorResponse(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+
+    public class ApiEmptyResponse<T> extends ApiResponse<T> {
+    }
+}
