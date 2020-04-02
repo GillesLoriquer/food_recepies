6a58570156ab896f8e9b4af6c55722d6a7297310
gilles loriquer <gilles.loriquer@gfi.fr>
2020-04-02 15:13:05 +0200

    Définition de Recipe en tant qu'entité Room

--- app/src/main/java/com/example/foodrecipes/model/Recipe.java
+++ app/src/main/java/com/example/foodrecipes/model/Recipe.java
@@ -4,37 +4,67 @@ package com.example.foodrecipes.model;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+@Entity(tableName = "recipes")
 public class Recipe implements Parcelable {
 
-    private String title;
-    private String publisher;
-    private List<String> ingredients;
+    /**
+     * -------------------------------- ATTRIBUTES
+     */
+    @PrimaryKey
+    @NonNull
     @SerializedName("recipe_id")
     private String recipeId;
+
+    private String title;
+
+    private String publisher;
+
+    private List<String> ingredients;
+
+    @ColumnInfo(name = "image_url")
     @SerializedName("image_url")
     private String imageUrl;
+
+    @ColumnInfo(name = "social_rank")
     @SerializedName("social_rank")
     private Double socialRank;
 
-    // Constructors
+    private int timestamp;
+
+    /**
+     * -------------------------------- CONSTRUCTORS
+     */
     public Recipe() {
         this.socialRank = 0.0D;
     }
 
-    public Recipe(String title, String publisher, List<String> ingredients, String recipeId, String imageUrl, Double socialRank) {
+    public Recipe(@NonNull String recipeId, String title, String publisher, List<String> ingredients, String imageUrl, Double socialRank, int timestamp) {
+        this.recipeId = recipeId;
         this.title = title;
         this.publisher = publisher;
         this.ingredients = ingredients;
-        this.recipeId = recipeId;
         this.imageUrl = imageUrl;
         this.socialRank = socialRank;
+        this.timestamp = timestamp;
     }
 
-    // Getters
+    /**
+     * -------------------------------- GETTERS
+     */
+    @NonNull
+    public String getRecipeId() {
+        return recipeId;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -47,10 +77,6 @@ public class Recipe implements Parcelable {
         return ingredients;
     }
 
-    public String getRecipeId() {
-        return recipeId;
-    }
-
     public String getImageUrl() {
         return imageUrl;
     }
@@ -59,7 +85,17 @@ public class Recipe implements Parcelable {
         return socialRank;
     }
 
-    // Setters
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * -------------------------------- SETTERS
+     */
+    public void setRecipeId(@NonNull String recipeId) {
+        this.recipeId = recipeId;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -72,10 +108,6 @@ public class Recipe implements Parcelable {
         this.ingredients = ingredients;
     }
 
-    public void setRecipeId(String recipeId) {
-        this.recipeId = recipeId;
-    }
-
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
@@ -84,30 +116,62 @@ public class Recipe implements Parcelable {
         this.socialRank = socialRank;
     }
 
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * -------------------------------- BUILT-IN METHODES
+     */
     @Override
     public String toString() {
         return "Recipe{" +
-                "title='" + title + '\'' +
+                "recipeId='" + recipeId + '\'' +
+                ", title='" + title + '\'' +
                 ", publisher='" + publisher + '\'' +
                 ", ingredients=" + ingredients +
-                ", recipeId='" + recipeId + '\'' +
                 ", imageUrl='" + imageUrl + '\'' +
                 ", socialRank=" + socialRank +
+                ", timestamp=" + timestamp +
                 '}';
     }
 
-    // Parcelable implementation
+    /**
+     * -------------------------------- PARCELABLE IMPLEMENTATION
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(recipeId);
+        dest.writeString(title);
+        dest.writeString(publisher);
+        dest.writeStringList(ingredients);
+        dest.writeString(imageUrl);
+        if (socialRank == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeDouble(socialRank);
+        }
+        dest.writeInt(timestamp);
+    }
+
     protected Recipe(Parcel in) {
+        recipeId = in.readString();
         title = in.readString();
         publisher = in.readString();
         ingredients = in.createStringArrayList();
-        recipeId = in.readString();
         imageUrl = in.readString();
         if (in.readByte() == 0) {
             socialRank = null;
         } else {
             socialRank = in.readDouble();
         }
+        timestamp = in.readInt();
     }
 
     public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
@@ -122,23 +186,4 @@ public class Recipe implements Parcelable {
         }
     };
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(title);
-        dest.writeString(publisher);
-        dest.writeStringList(ingredients);
-        dest.writeString(recipeId);
-        dest.writeString(imageUrl);
-        if (socialRank == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeDouble(socialRank);
-        }
-    }
 }
